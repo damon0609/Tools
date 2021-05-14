5c32b4659bf9814104760d36ca89494b86b0a27a
damon0609 <903005766@qq.com>
2021-05-15 07:32:43 +0800

    FSM功能

---
+++ FSM/FSM.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Damon.Tools
+{
+
+
+    public class FSM
+    {
+        public FSM() { }
+
+        private bool isPause = false;
+
+        private Dictionary<string, FSMState> states = new Dictionary<string, FSMState>();
+
+        public void OnResume()
+        {
+            isPause = false;
+        }
+        public void OnPause()
+        {
+            isPause = true;
+        }
+        public void Add(FSMState state)
+        {
+            if (states.ContainsKey(state.stateName))
+            {
+                states.Add(state.stateName, state);
+            }
+        }
+        public void Remove(FSMState state)
+        {
+            if (states.ContainsKey(state.stateName))
+            {
+                states.Remove(state.stateName);
+            }
+        }
+
+        public void Entity(FSMState state)
+        {
+            this.curState = state;
+            state.OnEtenr();
+        }
+
+        public event Action<FSMState> stateChangeEvent;
+
+        private FSMState _curState;
+        public FSMState curState
+        {
+            get => _curState;
+            set
+            {
+                if (_curState != value)
+                {
+                    _curState = value;
+                    if (stateChangeEvent != null)
+                    {
+                        stateChangeEvent.Invoke(value);
+                    }
+                }
+            }
+        }
+        public void Update()
+        {
+            if (curState != null && !isPause)
+            {
+                curState.Update();
+            }
+        }
+        public void ExitFsm()
+        {
+            states.Clear();
+            stateChangeEvent = null;
+            isPause = false;
+            _curState = null;
+        }
+    }
+}
+
---
+++ FSM/FSMState.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Damon.Tools
+{
+    public class FSMState
+    {
+        public event Action enterEvent;
+        public event Action exitEvent;
+
+        private FSM fsm;
+        private string mStateName;
+        public string stateName { get { return mStateName; } }
+
+        private Dictionary<FSMState, Func<bool>> states = new Dictionary<FSMState, Func<bool>>();
+
+        public FSMState UnRegister(FSMState state)
+        {
+            if (states.ContainsKey(state))
+            {
+                states.Remove(state);
+            }
+            return state;
+        }
+        public FSMState Register(FSMState state, Func<bool> func)
+        {
+            states.Add(state, func);
+            return this;
+        }
+        public FSMState(string name, FSM fsm)
+        {
+            this.mStateName = name;
+            this.fsm = fsm;
+        }
+        public void Update()
+        {
+
+            foreach (FSMState key in states.Keys)
+            {
+                if (states[key]())
+                {
+                    this.OnExit();
+                    fsm.curState = key;
+                    key.OnEtenr();
+                }
+            }
+        }
+
+        public void OnEtenr()
+        {
+            enterEvent?.Invoke();
+        }
+
+        public void OnExit()
+        {
+            exitEvent?.Invoke();
+        }
+    }
+}
